bf68ebc0df6923e71403b83fdd64f8749d5cb54a
nyyelin <nyiyelin4@gmail.com>
2022-01-28 00:37:09 +0630

    second commit

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h3>HOME PAGE</h3>
+    <p class="red green--text">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos cupiditate optio reprehenderit tempore laborum. Incidunt dignissimos accusantium cum tempore dolore? Illo, vero ad! Quod illo voluptate minus repellendus quo ad.
+    </p>
+    <p class="pink lighten-4 red--text text--darken-4">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos cupiditate optio reprehenderit tempore laborum. Incidunt dignissimos accusantium cum tempore dolore? Illo, vero ad! Quod illo voluptate minus repellendus quo ad.
+    </p>
+    <h1 class = "display-4">Hello Testing</h1>
+    <h3 class = "display-1">Hello Display 1</h3>
+    <p class = "headline">Hello headline</p>
+    <p class = "subtitle-1 font-weight-bold">Hello Testing</p>
+    <p class = "subtitle-2">Hello Testing</p>
+    <p class = "capton">Caption</p>
+    <v-btn class="pink white--text">Submit</v-btn>
+    <v-btn depressed class="pink">Submit</v-btn>
+    <v-btn flat color="green">Submit</v-btn>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
